ae862d7202a6fe01a7bcbd41dc462b399fe88396
derrickwaweru <derrickwaweru38@gmail.com>
2017-08-25 12:50:40 +0300

    added a success route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -8,11 +9,25 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-    get("/",(request,response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template","templates/index.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
+    get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("name", request.session().attribute("name"));
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      post("/stylists",(request,response) -> {
+        Map<String, Object> model= new HashMap<String, Object>();
+
+        String name= request.queryParams("name");
+        Stylist newStylist = new Stylist(name);
+        request.session().attribute("stylist, newStylist");
+
+        model.put("template","templates/success.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+
 
   }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -13,16 +13,4 @@ public void Stylist_instantiatesWithName_String(){
   Stylist myStylist = new Stylist("Aqua mist");
   assertEquals("Aqua mist",myStylist.getName());
 }
-
-post("/stylists",(request,response) -> {
-  Map<String, Object> model= new HashMap<String, Object>();
-
-  String name= request.queryParams("name");
-  Stylist newStylist = new Stylist(name);
-  request.session().attribute("stylist, newStylist");
-
-  model.put("template","templates/success.vtl");
-  return new ModelAndView(model, layout);
-}, new VelocityTemplateEngine());
-
 }
